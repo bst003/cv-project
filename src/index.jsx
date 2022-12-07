f7c3955f77683ef1296df990f0380f50429ccc33
bst003 <bstanner33@yahoo.com>
2022-12-06 23:25:31 -0500

    Begin writing notes for project

--- src/index.jsx
+++ src/index.jsx
@@ -3,5 +3,23 @@ import ReactDOM from "react-dom/client";
 
 import "./assets/scss/styles.scss";
 
+/*
+
+Style, multiple options
+
+    1. Form to the left and visual to the right
+
+    2. Just form fields that I can then generate into a cv
+
+    3. A more visual example that can be updated on click
+
+Data
+
+    General contact info could be single values in state
+
+    Positions and education would need to be arrays in the state, to allow for multiple options
+
+*/
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<React.StrictMode>test</React.StrictMode>);

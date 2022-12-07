ee57a1159d2b7a1d9b378d0c1a7ef49509e6024f
bst003 <bstanner33@yahoo.com>
2022-12-06 23:13:56 -0500

    Add scss stylesheet

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import "./assets/scss/styles.scss";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<React.StrictMode>test</React.StrictMode>);

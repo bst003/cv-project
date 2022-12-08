7f9c9f42f4972f4178c3527c3559c91b135210c9
bst003 <bstanner33@yahoo.com>
2022-12-07 23:58:29 -0500

    Update base css and narrow down notes

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+// App.js
+
+import React, { Component } from "react";
+// import uniqid from "uniqid";
+
+class App extends Component {
+    constructor() {
+        super();
+
+        this.state = {};
+    }
+
+    render() {
+        // const { task, tasks } = this.state;
+
+        return <div>Hello world</div>;
+    }
+}
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import App from "./App";
+
 import "./assets/scss/styles.scss";
 
 /*
@@ -8,18 +10,20 @@ import "./assets/scss/styles.scss";
 Style, multiple options
 
     1. Form to the left and visual to the right
-
     2. Just form fields that I can then generate into a cv
-
-    3. A more visual example that can be updated on click
+    ***3. A more visual example that can be updated on click***
 
 Data
 
-    General contact info could be single values in state
+    - General contact info could be single values in state
 
-    Positions and education would need to be arrays in the state, to allow for multiple options
+    - Positions and education would need to be arrays in the state, to allow for multiple options
 
 */
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<React.StrictMode>test</React.StrictMode>);
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);

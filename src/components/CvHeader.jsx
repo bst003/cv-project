c0fd1bbbe96c53de918a51ac15a90782f83c71d8
bst003 <bstanner33@yahoo.com>
2023-01-09 22:47:52 -0500

    Begin refactor of CV Header

--- src/components/CvHeader.jsx
+++ src/components/CvHeader.jsx
@@ -1,72 +1,55 @@
 import "./CvHeader.scss";
 
-import React, { Component } from "react";
+import React, { useState } from "react";
 import uniqid from "uniqid";
 
 import FormField from "./FormField";
 
-/*
+const CvHeader = (props) => {
+    const [editing, setEditing] = useState(false);
+    const [name, setName] = useState([
+        {
+            value: "Benjamin Smith",
+            inputType: "text",
+            uniqid: uniqid(),
+        },
+    ]);
+    const [title, setTitle] = useState([
+        {
+            value: "Full Stack Web Developer",
+            inputType: "text",
+            uniqid: uniqid(),
+        },
+    ]);
+    const [contactMethods, setContactMethods] = useState([
+        {
+            value: "111-222-3333",
+            name: "Phone",
+            inputType: "tel",
+            uniqid: uniqid(),
+        },
+        {
+            value: "placeholder@gmail.com",
+            name: "Email",
+            inputType: "email",
+            uniqid: uniqid(),
+        },
+        {
+            value: "testportfolio.com",
+            name: "Portfolio",
+            inputType: "text",
+            uniqid: uniqid(),
+        },
+    ]);
+    const [bio, setBio] = useState([
+        {
+            value: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam cursus est sed hendrerit rhoncus. Nam sit amet lectus a ipsum euismod viverra non eu tortor. In hac habitasse platea dictumst.",
+            inputType: "textarea",
+            uniqid: uniqid(),
+        },
+    ]);
 
-Streamline variable/key names!!!
-
-*/
-
-class CvHeader extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            editing: false,
-            name: [
-                {
-                    value: "Benjamin Smith",
-                    inputType: "text",
-                    uniqid: uniqid(),
-                },
-            ],
-            title: [
-                {
-                    value: "Full Stack Web Developer",
-                    inputType: "text",
-                    uniqid: uniqid(),
-                },
-            ],
-            contactMethods: [
-                {
-                    value: "111-222-3333",
-                    name: "Phone",
-                    inputType: "tel",
-                    uniqid: uniqid(),
-                },
-                {
-                    value: "placeholder@gmail.com",
-                    name: "Email",
-                    inputType: "email",
-                    uniqid: uniqid(),
-                },
-                {
-                    value: "testportfolio.com",
-                    name: "Portfolio",
-                    inputType: "text",
-                    uniqid: uniqid(),
-                },
-            ],
-            bio: [
-                {
-                    value: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam cursus est sed hendrerit rhoncus. Nam sit amet lectus a ipsum euismod viverra non eu tortor. In hac habitasse platea dictumst.",
-                    inputType: "textarea",
-                    uniqid: uniqid(),
-                },
-            ],
-        };
-
-        this.editInfo = this.editInfo.bind(this);
-        this.onInputChange = this.onInputChange.bind(this);
-        this.onContactInputChange = this.onContactInputChange.bind(this);
-        this.submitInfo = this.submitInfo.bind(this);
-    }
-
-    onInputChange(e) {
+    const onInputChange = (e) => {
         const fieldData = {
             value: e.currentTarget.value,
             inputType: e.currentTarget.getAttribute("type"),
@@ -82,17 +65,15 @@ class CvHeader extends Component {
         this.setState({
             [key]: [fieldData],
         });
-    }
+    };
 
-    onContactInputChange(e) {
+    const onContactInputChange = (e) => {
         const fieldData = {
             value: e.currentTarget.value,
             name: e.currentTarget.getAttribute("name"),
             inputType: e.currentTarget.getAttribute("type"),
             uniqid: e.currentTarget.getAttribute("data-uniqid"),
         };
-
-        const { contactMethods } = this.state;
 
         console.log(contactMethods);
 
@@ -120,113 +101,135 @@ class CvHeader extends Component {
                 console.log(contactMethods);
             }
         );
-    }
+    };
 
-    editInfo(e) {
-        this.setState({
-            editing: true,
-        });
-    }
+    const editInfo = (e) => {
+        setEditing(true);
+    };
 
-    submitInfo(e) {
+    const submitInfo = (e) => {
         e.preventDefault();
-        this.setState({
-            editing: false,
-        });
-    }
+        setEditing(false);
+    };
 
-    render() {
-        const { editing, name, title, contactMethods, bio } = this.state;
+    const contents = () => {
+        let contentsInfo;
+        const contentsArray = [];
 
-        const contents = () => {
-            let contentsInfo;
-            const contentsArray = [];
+        const stateObj = { name, title, contactMethods, bio };
 
-            if (editing) {
-                Object.entries(this.state).forEach(([key, value]) => {
-                    if (key !== "editing" && key !== "contactMethods") {
-                        contentsArray.push(
+        if (editing) {
+            // Object.entries(stateObj).forEach(([key, value]) => {
+            //     console.log(`${key}: ${value}`);
+            //     contentsArray.push(
+            //         <FormField
+            //             key={value[0].uniqid}
+            //             onInputChanged={onInputChange}
+            //             inputName={key}
+            //             inputUniqid={value[0].uniqid}
+            //             inputValue={value[0].value}
+            //             inputType={value[0].inputType}
+            //         />
+            //     );
+            // });
+            // for (let i = 0; i < stateArray.length; i++) {
+            //     console.log(stateArray[i]);
+
+            //     contentsArray.push(
+            //         <FormField
+            //             key={stateArray[0].uniqid}
+            //             onInputChanged={onInputChange}
+            //             inputName={key}
+            //             inputUniqid={value[0].uniqid}
+            //             inputValue={value[0].value}
+            //             inputType={value[0].inputType}
+            //         />
+            //     );
+            // }
+
+            Object.entries(stateObj).forEach(([key, value]) => {
+                if (key !== "contactMethods") {
+                    contentsArray.push(
+                        <FormField
+                            key={value[0].uniqid}
+                            onInputChanged={onInputChange}
+                            inputName={key}
+                            inputUniqid={value[0].uniqid}
+                            inputValue={value[0].value}
+                            inputType={value[0].inputType}
+                        />
+                    );
+                }
+
+                if (key === "contactMethods") {
+                    const contactFields = [];
+
+                    value.forEach((contactMethod) => {
+                        contactFields.push(
                             <FormField
-                                key={value[0].uniqid}
-                                onInputChanged={this.onInputChange}
-                                inputName={key}
-                                inputUniqid={value[0].uniqid}
-                                inputValue={value[0].value}
-                                inputType={value[0].inputType}
+                                key={contactMethod.uniqid}
+                                onInputChanged={onContactInputChange}
+                                inputName={contactMethod.name}
+                                inputValue={contactMethod.value}
+                                inputUniqid={contactMethod.uniqid}
+                                inputType={contactMethod.inputType}
                             />
                         );
-                    }
+                    });
 
-                    if (key === "contactMethods") {
-                        const contactFields = [];
+                    contentsArray.push(contactFields);
+                }
+            });
 
-                        value.forEach((contactMethod) => {
-                            contactFields.push(
-                                <FormField
-                                    key={contactMethod.uniqid}
-                                    onInputChanged={this.onContactInputChange}
-                                    inputName={contactMethod.name}
-                                    inputValue={contactMethod.value}
-                                    inputUniqid={contactMethod.uniqid}
-                                    inputType={contactMethod.inputType}
-                                />
-                            );
-                        });
-
-                        contentsArray.push(contactFields);
-                    }
-                });
-
-                contentsInfo = (
-                    <form className="cv-form" onSubmit={editing ? this.submitInfo : null}>
-                        {contentsArray}
-                        <footer className="cv-form__footer">
-                            <button className="btn" type="submit">
-                                Save
-                            </button>
-                        </footer>
-                    </form>
-                );
-            } else {
-                const contactFields = [];
-
-                contactMethods.forEach((contactMethod) => {
-                    contactFields.push(
-                        <li className="contact-type" key={contactMethod.uniqid}>
-                            {contactMethod.value}
-                        </li>
-                    );
-                });
-
-                contentsInfo = (
-                    <div>
-                        <button
-                            onClick={!editing ? this.editInfo : null}
-                            className="btn icon ctrl"
-                            type="button"
-                        >
-                            <i className="fas fa-edit" />
+            contentsInfo = (
+                <form className="cv-form" onSubmit={editing ? submitInfo : null}>
+                    {contentsArray}
+                    <footer className="cv-form__footer">
+                        <button className="btn" type="submit">
+                            Save
                         </button>
-                        <div className="cvh__info direct-wrap">
-                            <h2 className="cvh__name">{name[0].value}</h2>
-                            <p className="cvh__title">{title[0].value}</p>
-                            <ul className="cvh__cm">{contactFields}</ul>
-                            <div className="cvh__bio direct-wrap">
-                                <h3 className="cv-title">
-                                    <span>Professional Summary</span>
-                                </h3>
-                                <p className="cvh__bio">{bio[0].value}</p>
-                            </div>
+                    </footer>
+                </form>
+            );
+        } else {
+            const contactFields = [];
+
+            contactMethods.forEach((contactMethod) => {
+                contactFields.push(
+                    <li className="contact-type" key={contactMethod.uniqid}>
+                        {contactMethod.value}
+                    </li>
+                );
+            });
+
+            contentsInfo = (
+                <div>
+                    <button
+                        onClick={!editing ? editInfo : null}
+                        className="btn icon ctrl"
+                        type="button"
+                    >
+                        <i className="fas fa-edit" />
+                    </button>
+                    <div className="cvh__info direct-wrap">
+                        <h2 className="cvh__name">{name[0].value}</h2>
+                        <p className="cvh__title">{title[0].value}</p>
+                        <ul className="cvh__cm">{contactFields}</ul>
+                        <div className="cvh__bio direct-wrap">
+                            <h3 className="cv-title">
+                                <span>Professional Summary</span>
+                            </h3>
+                            <p className="cvh__bio">{bio[0].value}</p>
                         </div>
                     </div>
-                );
-            }
+                </div>
+            );
+        }
 
-            return contentsInfo;
-        };
+        return contentsInfo;
+    };
 
-        return <div className="cvh cv-sec">{contents()}</div>;
-    }
-}
+    return <div className="cvh cv-sec">{contents()}</div>;
+};
 
 export default CvHeader;
